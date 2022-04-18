7b3c09a9afd58cfe0721a5b845810c694e6baa82
JameyWoo <2622075127wjh@gmail.com>
2022-04-18 22:32:11 +0800

    实现了lab1P0

--- tinykv/kv/storage/standalone_storage/standalone_storage.go
+++ tinykv/kv/storage/standalone_storage/standalone_storage.go
@@ -27,16 +27,24 @@ func (s *StandAloneStorage) Stop() error {
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// YOUR CODE HERE (lab1).
-	panic("not implemented yet")
-	return nil, nil
+	return NewBadgerReader(s.db.NewTransaction(false)), nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// YOUR CODE HERE (lab1).
 	// Try to check the definition of `storage.Modify` and txn interface of `badger`.
 	// As the column family is not supported by `badger`, a wrapper is used to simulate it.
-	panic("not implemented yet")
-	return nil
+	wb := new(engine_util.WriteBatch)
+	for i := 0; i < len(batch); i++ {
+		switch batch[i].Data.(type) {
+		case storage.Put:
+			wb.SetCF(batch[i].Cf(), batch[i].Key(), batch[i].Value())
+		case storage.Delete:
+			wb.DeleteCF(batch[i].Cf(), batch[i].Key())
+		}
+	}
+	err := wb.WriteToDB(s.db)
+	return err
 }
 
 type BadgerReader struct {
